48c30e3bc71859f8f3a0281127209e5a5de2ce92
Ung Nhat Hang <hang.ung@executionlab.asia>
2022-05-31 11:55:04 +0700

    first commit

---
+++ compile.js
@@ -0,0 +1,11 @@
+// compile code will go here
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf-8");
+
+// console.log(solc.compile(source, 1).contracts[':Inbox'].interface);
+
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
